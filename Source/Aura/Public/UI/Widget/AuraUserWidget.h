c3c50a99947c0961238ea8436d58029274abcd4f
Shozzy <shozzy95@gmail.com>
2025-05-26 23:20:29 +0300

    Overlay Widget Controller

--- Source/Aura/Public/UI/Widget/AuraUserWidget.h
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -16,6 +16,7 @@ class AURA_API UAuraUserWidget : public UUserWidget
 public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetController(UObject* InWidgetController);
+	
 	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<UObject> WidgetController;
 protected:
